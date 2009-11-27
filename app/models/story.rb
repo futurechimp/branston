8cb22182e8446e13b1fa5725f19bb05f7e06e168
dave@boomer <dave@boomer>
2009-11-27 15:16:29 +0000

    Story belongs to iteration.

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,12 @@
 class Story < ActiveRecord::Base
 
+  # Validations
+  #
   validates_presence_of :description, :points
+
+  # Associations
+  #
+  belongs_to :iteration
 
 end
 
--- test/unit/story_test.rb
+++ test/unit/story_test.rb
@@ -4,5 +4,7 @@ class StoryTest < ActiveSupport::TestCase
 
   should_validate_presence_of :description, :points
 
+  should_belong_to :iteration
+
 end
 
