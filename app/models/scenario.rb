0c12247e2a041312e56ecf7d27f8cc9471c12527
dave@boomer <dave@boomer>
2009-11-27 17:52:34 +0000

    Fixing a goofy mistake with associations.

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -3,7 +3,6 @@ class Scenario < ActiveRecord::Base
   belongs_to :story
   has_many :outcomes
   has_many :preconditions
-  has_many :scenarios
 
 end
 
--- app/models/story.rb
+++ app/models/story.rb
@@ -4,20 +4,21 @@ class Story < ActiveRecord::Base
   #
   validates_presence_of :description, :points, :title
   validates_uniqueness_of :title
-  
+
   # Associations
   #
   belongs_to :iteration
   belongs_to :author, :class_name => "User", :foreign_key => "author_id"
+  has_many :scenarios
 
   # Named scopes
   #
   named_scope :in_progress, :conditions => ['iteration_id IS NOT ?', nil]
-  
+
   def feature_filename
     title.parameterize('_').to_s + '.feature'
   end
-  
+
   def make_feature
     gherkin = "Feature: #{title}\n"
     File.open(FEATURE_PATH + feature_filename, 'w') {|f| f.write(gherkin) }
