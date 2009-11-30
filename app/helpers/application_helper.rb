6680c8fa07fe8dc0f91e8aaabe156a1927ca3f68
Steve <steve.laing@gmail.com>
2009-11-30 10:53:36 +0000

    More layout tweaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  # Applies an html class attribute where the controller name contains the 'tab_name'.
+  def tab_on(tab_name, html_class="current_page_item")
+    controller_name.match(Regexp.new("#{tab_name}")) ? " class=\"#{html_class}\"" : ''
+  end
 end
+
