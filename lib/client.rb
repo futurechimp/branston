dce09b098a1cd092f94b91ffd43f6b52a76224eb
dmgarland <dan@dangarland.co.uk>
2009-12-03 16:17:53 +0000

    Changed branston binary to accept either server or generate switches that run the branston server or generates the cucumber files given a host, port and URL

--- lib/client.rb
+++ lib/client.rb
@@ -5,40 +5,51 @@ require 'lib/story_generator'
 include StoryGenerator
 
 class Client
+  
+  attr_accessor :args
+  
+  def initialize(*args)
+   # puts args
+    self.args = args
+    generate_story_files
+  end
+  
+  def generate_story_files
 
-  Net::HTTP.start('192.168.0.158' , 3000) {|http|
-    req = Net::HTTP::Get.new('/stories/2.xml')
-    response = http.request(req)
-    xml = REXML::Document.new response.body
-    #puts xml
-    root = xml.root
-    story = OpenStruct.new
-    story.description = root.elements["/story/description"].text
-    story.title = root.elements["/story/title"].text
-    story.scenarios = []
-    root.elements.each("/story/scenarios") { |scenario|
-      s = OpenStruct.new
-      s.preconditions = []
-      s.outcomes = []
-      s.title = scenario.elements["scenario/title"].text
-
-      scenario.elements.each("scenario/preconditions/precondition") { |precondition|
-        p = OpenStruct.new
-        p.description = precondition.elements["description"].text
-        s.preconditions << p
+    Net::HTTP.start(args[1] , args[2]) {|http|
+      req = Net::HTTP::Get.new(args[3])
+      response = http.request(req)
+      xml = REXML::Document.new response.body
+      #puts xml
+      root = xml.root
+      story = OpenStruct.new
+      story.description = root.elements["/story/description"].text
+      story.title = root.elements["/story/title"].text
+      story.scenarios = []
+      root.elements.each("/story/scenarios") { |scenario|
+        s = OpenStruct.new
+        s.preconditions = []
+        s.outcomes = []
+        s.title = scenario.elements["scenario/title"].text
+        
+        scenario.elements.each("scenario/preconditions/precondition") { |precondition|
+          p = OpenStruct.new
+          p.description = precondition.elements["description"].text
+          s.preconditions << p
+        }
+        
+        scenario.elements.each("scenario/outcomes/outcome") { |outcome|
+          o = OpenStruct.new
+          o.description = outcome.elements["description"].text
+          s.outcomes << o
+        }
+        
+        story.scenarios << s
       }
-
-      scenario.elements.each("scenario/outcomes/outcome") { |outcome|
-        o = OpenStruct.new
-        o.description = outcome.elements["description"].text
-        s.outcomes << o
-      }
-
-      story.scenarios << s
+      
+      generate(story)
     }
-
-    generate(story)
-  }
+  end
 
 end
 
