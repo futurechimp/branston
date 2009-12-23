1b2ddb57309f2f091a75a72c676bb2db82bad98f
dmgarland <dan@dangarland.co.uk>
2009-12-23 14:37:39 +0000

    Restored generate story link

--- lib/branston/config/routes.rb
+++ lib/branston/config/routes.rb
@@ -5,7 +5,7 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :releases
   map.resources :iterations, :except => 'show' do |i|
-    i.resources :stories do |r|
+    i.resources :stories, :member => { :generate_feature => :get } do |r|
       r.resources :scenarios
     end
   end
