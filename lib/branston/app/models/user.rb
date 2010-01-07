72517cf336c65a8aaeacfbf01834f41eda80c318
dave@boomer <dave@boomer>
2010-01-07 15:31:14 +0000

    Adding the AGPL to the project.

--- lib/branston/app/models/user.rb
+++ lib/branston/app/models/user.rb
@@ -1,3 +1,17 @@
+#    This file is part of Branston.
+#
+#    Branston is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation.
+#
+#    Branston is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with Branston.  If not, see <http://www.gnu.org/licenses/>.
+
 require 'digest/sha1'
 
 class User < ActiveRecord::Base
@@ -31,7 +45,7 @@ class User < ActiveRecord::Base
 
   # Authenticates a user by their login name and unencrypted password.  Returns the user or nil.
   #
-  # uff.  this is really an authorization, not authentication routine.  
+  # uff.  this is really an authorization, not authentication routine.
   # We really need a Dispatch Chain here or something.
   # This will also let us return a human error message.
   #
@@ -48,13 +62,14 @@ class User < ActiveRecord::Base
   def email=(value)
     write_attribute :email, (value ? value.downcase : nil)
   end
-  
+
   def to_s
     login
   end
 
   protected
-    
+
 
 
 end
+
