ade5b021cc0049b733ea7d0994add2e3c7fa1e29
dave@boomer <dave@boomer>
2010-01-07 17:32:32 +0000

    Added some filter exceptions so that generating features works.

--- lib/branston/app/controllers/stories_controller.rb
+++ lib/branston/app/controllers/stories_controller.rb
@@ -17,7 +17,7 @@ class StoriesController < ApplicationController
   layout 'main'
   before_filter :login_required, :except => [:show, :generate_feature]
   before_filter :retrieve_iterations, :except => [:generate_feature]
-  before_filter :load_iteration
+  before_filter :load_iteration, :except => [:generate_feature, :show]
 
   in_place_edit_for :story, :title
   in_place_edit_for :story, :description
