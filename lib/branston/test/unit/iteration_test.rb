2724ede7004b747cc80e1694e96752dc14085f2c
dave <dave@netbook>
2010-07-26 16:41:35 +0100

    Tested the Iteration's burndown_data method a bit.

--- lib/branston/test/unit/iteration_test.rb
+++ lib/branston/test/unit/iteration_test.rb
@@ -10,5 +10,33 @@ class IterationTest < ActiveSupport::TestCase
   should_have_many :participations
   should_belong_to :release
 
+  context "An Iterations's burndown data method" do
+
+    context "for incomplete stories" do
+
+      setup do
+        @iteration = Iteration.make
+        Story.make(:points => 5, :iteration => @iteration)
+        Story.make(:points => 2, :iteration => @iteration)
+      end
+
+      should "return an empty array" do
+        assert_equal @iteration.burndown_data, []
+      end
+    end
+
+    context "for completed stories" do
+      setup do
+        @iteration = Iteration.make
+        Story.make(:points => 5, :iteration => @iteration, :status => "completed")
+        Story.make(:points => 2, :iteration => @iteration, :status => "completed")
+      end
+
+      should "return a points value" do
+        assert_match /7/, @iteration.burndown_data.to_s
+      end
+    end
+  end
+
 end
 
