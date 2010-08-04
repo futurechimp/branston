439a8acbff9931c918ae7303433a95d6e928c718
dave <dave@netbook>
2010-08-04 14:30:25 +0100

    Some more security testing.

--- lib/branston/test/functional/iterations_controller_test.rb
+++ lib/branston/test/functional/iterations_controller_test.rb
@@ -3,23 +3,45 @@ require 'test_helper'
 class IterationsControllerTest < ActionController::TestCase
 
   context "The IterationsController" do
-
     setup do
       @user = User.make
     end
 
-    context "when there are no iterations yet" do
-      context "when the user is not logged in" do
-        setup do
-          get :index
-        end
+    context "when the user is not logged in" do
+      [:index, :new, :edit].each do |action|
+        context "on GET to #{action.to_s}" do
+          setup do
+            get action
+          end
 
+          should "redirect to login" do
+            assert_redirected_to new_session_path
+          end
+        end
+      end
+
+      context "on PUT to :update" do
+        setup do
+          put :update
+        end
         should "redirect to login" do
           assert_redirected_to new_session_path
         end
       end
 
-      context "when the user is logged in" do
+      context "on POST to :create" do
+        setup do
+          post :create
+        end
+        should "redirect to login" do
+          assert_redirected_to new_session_path
+        end
+      end
+
+    end
+
+    context "when the user is logged in" do
+      context "when there are no iterations yet" do
         setup do
           login_as(@user)
           get :index
@@ -33,131 +55,144 @@ class IterationsControllerTest < ActionController::TestCase
     end
 
     context "when at least one iteration exists" do
-      setup do
-        login_as(@user)
-        @iteration = Iteration.make
-      end
-
-      should "get index" do
-        get :index
-        assert_response :success
-        assert_not_nil assigns(:iterations)
-      end
-
-      should "get edit" do
-        get :edit, :id => @iteration.to_param
-        assert_response :success
-        assert assigns(:releases)
-      end
-
-      should "get new" do
-        get :new
-        assert_response :success
-        assert assigns(:releases)
-      end
-
-      should "show iteration" do
-        get :show, :id => @iteration.to_param
-        assert_response :success
-      end
-
-      should "destroy iteration" do
-        assert_difference('Iteration.count', -1) do
-          delete :destroy, :id => @iteration.to_param
+      context "when the user is logged in" do
+        setup do
+          login_as(@user)
+          @iteration = Iteration.make
         end
 
-        assert_redirected_to iterations_path
-      end
-
-      context "creating an iteration" do
-        context "with valid params" do
+        context "on GET to index" do
           setup do
-            login_as(@user)
-            assert_difference("Iteration.count") do
-              post :create, :iteration => Iteration.plan
-            end
+            get :index
           end
 
-          should "not be assigned to a release" do
-            assert assigns(:iteration).release.nil?
+          should "work" do
+            assert_response :success
+            assert_not_nil assigns(:iterations)
+          end
+        end
+
+        should "get edit" do
+          get :edit, :id => @iteration.to_param
+          assert_response :success
+          assert assigns(:releases)
+        end
+
+        should "get new" do
+          get :new
+          assert_response :success
+          assert assigns(:releases)
+        end
+
+
+        context "on GET to show" do
+          setup do
+            get :show, :id => @iteration.to_param
+          end
+          should "show iteration" do
+            assert_response :success
+          end
+        end
+
+        should "destroy iteration" do
+          assert_difference('Iteration.count', -1) do
+            delete :destroy, :id => @iteration.to_param
           end
 
-          should "redirect to show" do
-            assert_redirected_to iterations_path
-          end
+          assert_redirected_to iterations_path
+        end
 
-          context "including a release_id" do
+
+        context "creating an iteration" do
+          context "with valid params" do
             setup do
+              login_as(@user)
               assert_difference("Iteration.count") do
-                post :create, :iteration => Iteration.plan.merge(:release_id => Release.make.to_param)
+                post :create, :iteration => Iteration.plan
               end
             end
 
-            should "be assigned to a release" do
-              assert assigns(:iteration).release
+            should "not be assigned to a release" do
+              assert assigns(:iteration).release.nil?
             end
-          end
-        end
 
-        context "with invalid params" do
-          setup do
-            login_as(@user)
-            assert_no_difference("Iteration.count") do
-              post :create, :iteration => {}
+            should "redirect to show" do
+              assert_redirected_to iterations_path
             end
-          end
 
-          should "redisplay" do
-            assert_template 'new'
-          end
+            context "including a release_id" do
+              setup do
+                assert_difference("Iteration.count") do
+                  post :create, :iteration => Iteration.plan.merge(:release_id => Release.make.to_param)
+                end
+              end
 
-          should "retrieve all releases" do
-            assert assigns(:releases)
-          end
-        end
-      end
-
-      context "updating an iteration" do
-        context "with valid parameters" do
-          setup do
-            login_as(@user)
-            assert_no_difference("Iteration.count") do
-              put :update,{ :id => @iteration.to_param,  :iteration => {:name => "bar"}}
+              should "be assigned to a release" do
+                assert assigns(:iteration).release
+              end
             end
           end
 
-          should "redirect to show" do
-            assert_redirected_to iterations_path
-          end
-
-          context "including a release_id" do
+          context "with invalid params" do
             setup do
-              put :update, :id => @iteration.to_param, :iteration => {:release_id => Release.make.to_param}
+              login_as(@user)
+              assert_no_difference("Iteration.count") do
+                post :create, :iteration => {}
+              end
             end
 
-            should "be assigned to a release" do
-              assert assigns(:iteration).release
+            should "redisplay" do
+              assert_template 'new'
+            end
+
+            should "retrieve all releases" do
+              assert assigns(:releases)
             end
           end
         end
 
-        context "with invalid parameters" do
-          setup do
-            put :update, :id => @iteration.id, :iteration => {:name => ""}
+        context "updating an iteration" do
+          context "with valid parameters" do
+            setup do
+              login_as(@user)
+              assert_no_difference("Iteration.count") do
+                put :update,{ :id => @iteration.to_param,  :iteration => {:name => "bar"}}
+              end
+            end
+
+            should "redirect to show" do
+              assert_redirected_to iterations_path
+            end
+
+            context "including a release_id" do
+              setup do
+                put :update, :id => @iteration.to_param, :iteration => {:release_id => Release.make.to_param}
+              end
+
+              should "be assigned to a release" do
+                assert assigns(:iteration).release
+              end
+            end
           end
 
-          should "redisplay the edit template" do
-            assert_template "edit"
-          end
+          context "with invalid parameters" do
+            setup do
+              put :update, :id => @iteration.id, :iteration => {:name => ""}
+            end
 
-          should "retrieve all releases" do
-            assert assigns(:releases)
+            should "redisplay the edit template" do
+              assert_template "edit"
+            end
+
+            should "retrieve all releases" do
+              assert assigns(:releases)
+            end
           end
         end
       end
     end
 
-    context "showing an iteration" do
+    context "showing an iteration with 5 stories" do
       setup do
         login_as(@user)
         @iteration = Iteration.make
@@ -167,10 +202,17 @@ class IterationsControllerTest < ActionController::TestCase
         get :show, :id => @iteration.to_param
       end
 
-      should "successfully respond with iteration data" do
+      should "work" do
         assert_response :success
+      end
+
+      should "successfully respond with iteration data" do
         assert assigns(:iteration)
         assert assigns(:iteration_data)
+      end
+
+      should "have 5 stories assigned to it" do
+        assert_equal assigns(:iteration).stories.length, 5
       end
     end
   end
