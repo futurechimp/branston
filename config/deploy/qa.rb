ee27db918740811845791f39038504c5c90cb3d4
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2011-09-07 13:21:11 +0100

    changing deploy file, we should pull these out of git though

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -5,6 +5,6 @@ role :db,  "branston.headlondon.office", :primary => true
 set :scm, "git"
 set :user, "capistrano"
 set :deploy_to, "/var/www/branston.headlondon.office/"
-set :repository, "git@github.com:sleepingstu/branston.git"
+set :repository, "git@github.com:futurechimp/branston.git"
 set :rails_env, :qa
 
